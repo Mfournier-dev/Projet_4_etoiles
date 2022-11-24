3a0f508993df04066b0c33410cc61e95b0b7382a
PaolaAGM <paorap.03@hotmail.com>
2022-11-23 19:09:55 -0500

    Creation des classes

---
+++ DataAccess/DAO/TablesDAO.cs
@@ -0,0 +1,16 @@
+﻿using Projet_4_etoiles.DataAccess.Context;
+using Projet_4_etoiles.DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_4_etoiles.DataAccess.DAO;
+public class TablesDAO : IDAO<TablesDTO>
+{
+	public Table()
+	{
+
+	}
+}
